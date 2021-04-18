ae76edfd1925fd722a08a4d8848cd20926552ed3
Serg-SS <serg_ss@ukr.net>
2021-04-19 00:17:47 +0300

    added gallery-main.js

---
+++ gallery-main.js
@@ -0,0 +1,8 @@
+import images from "./gallery-items";
+
+const refs = {
+    gallery: document.querySelector('.js-gallery'),
+    modal: document.querySelector('js-lightbox'),
+    modalImg: document.querySelector('lightbox__image'),
+}
+console.log(refs.gallery);
